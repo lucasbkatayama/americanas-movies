57c7cf9578cbb347c599852257c965a9ba7200d3
Lucas Katayama <lucas.katayama@allin.com.br>
2022-10-23 17:18:51 -0300

    refactor: move HttpGetClientSpy to its own file

---
+++ src/data/test/mock-http-client.ts
@@ -0,0 +1,10 @@
+import { HttpGetClient } from '../protocols/http/http-get-client'
+
+export class HttpGetClientSpy implements HttpGetClient {
+  url?: string
+
+  async get (url: string): Promise<void> {
+    this.url = url
+    return await Promise.resolve()
+  }
+}
--- src/data/usecases/search-movies/remote-search-movies.spec.ts
+++ src/data/usecases/search-movies/remote-search-movies.spec.ts
@@ -1,16 +1,8 @@
-import { HttpGetClient } from '@/data/protocols/http/http-get-client'
+import { HttpGetClientSpy } from '@/data/test/mock-http-client'
 import { RemoteSearchMovies } from './remote-search-movies'
 
 describe('RemoteSearchMovies', () => {
   test('Should call HttpGetClient with correct URL', async () => {
-    class HttpGetClientSpy implements HttpGetClient {
-      url?: string
-
-      async get (url: string): Promise<void> {
-        this.url = url
-        return await Promise.resolve()
-      }
-    }
     const url = 'any_url'
     const httpGetClientSpy = new HttpGetClientSpy()
     const sut = new RemoteSearchMovies(url, httpGetClientSpy)
