55c292dff5a5f530d90b350babe7326e42b61e95
Lucas Katayama <lucas.katayama@allin.com.br>
2022-10-23 18:07:36 -0300

    refactor: move SUT creation to a factory method

--- src/data/usecases/search-movies/remote-search-movies.spec.ts
+++ src/data/usecases/search-movies/remote-search-movies.spec.ts
@@ -1,11 +1,24 @@
 import { HttpGetClientSpy } from '@/data/test/mock-http-client'
 import { RemoteSearchMovies } from './remote-search-movies'
 
+type SutTypes = {
+  sut: RemoteSearchMovies
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url: string = 'any_url'): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemoteSearchMovies(url, httpGetClientSpy)
+  return {
+    sut,
+    httpGetClientSpy
+  }
+}
+
 describe('RemoteSearchMovies', () => {
   test('Should call HttpGetClient with correct URL', async () => {
-    const url = 'any_url'
-    const httpGetClientSpy = new HttpGetClientSpy()
-    const sut = new RemoteSearchMovies(url, httpGetClientSpy)
+    const url = 'other_url'
+    const { sut, httpGetClientSpy } = makeSut(url)
     await sut.search()
     expect(httpGetClientSpy.url).toBe(url)
   })
