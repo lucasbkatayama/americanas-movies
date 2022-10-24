07a1931943815c6ab9a888ffb17c4a5ae433cbea
Lucas Katayama <lucas.katayama@allin.com.br>
2022-10-24 00:04:06 -0300

    feat: ensure AxiosAdapter calls axios with correct verb

--- src/infra/http/axios-adapter/axios-adapter.ts
+++ src/infra/http/axios-adapter/axios-adapter.ts
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 export class AxiosAdapter {
   async get (params: HttpGetParams<any>): Promise<void> {
-    await axios(params.url)
+    await axios.get(params.url)
   }
 }
