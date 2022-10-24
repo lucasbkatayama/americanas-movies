e22a1fa26a89f61dd348650eb5dece0c79ffec6a
Lucas Katayama <lucas.katayama@allin.com.br>
2022-10-24 09:21:05 -0300

    test: ensure AxiosAdapter returns correct values

--- src/infra/http/axios-adapter/axios-adapter.spec.ts
+++ src/infra/http/axios-adapter/axios-adapter.spec.ts
@@ -5,6 +5,11 @@ import { HttpGetParams } from '@/data/protocols/http'
 
 jest.mock('axios')
 const mockedAxios = axios as jest.Mocked<typeof axios>
+const mockedAxiosResult = {
+  data: faker.random.word,
+  status: faker.random.numeric(3)
+}
+mockedAxios.get.mockResolvedValue(mockedAxiosResult)
 
 const makeSut = (): AxiosAdapter => {
   return new AxiosAdapter()
@@ -21,5 +26,14 @@ describe('AxiosAdapter', () => {
     const sut = makeSut()
     await sut.get(request)
     expect(mockedAxios.get).toHaveBeenCalledWith(request.url, request.params)
+  })
+
+  test('Should return correct statusCode and body', async () => {
+    const sut = makeSut()
+    const httpResponse = await sut.get(mockGetRequest())
+    expect(httpResponse).toEqual({
+      statusCode: mockedAxiosResult.status,
+      body: mockedAxiosResult.data
+    })
   })
 })
