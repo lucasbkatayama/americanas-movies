5293b4771d61c1f7e65973a5a5d73883b3d2b609
Lucas Katayama <lucas.katayama@allin.com.br>
2022-10-24 20:17:44 -0300

    test: ensure Search calls SearchMovies with correct values

--- src/presentation/pages/search/search.spec.tsx
+++ src/presentation/pages/search/search.spec.tsx
@@ -3,18 +3,34 @@ import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react
 import Search from './search'
 import { ValidationSpy } from '@/presentation/test/mock-validation'
 import { faker } from '@faker-js/faker'
+import { SearchMovies, SearchMoviesParams } from '@/domain/usecases'
+import { TinyMovieModel } from '@/domain/models'
+import { mockTinyMovieModel } from '@/domain/test'
+
+export class SearchMoviesSpy implements SearchMovies {
+  movies = mockTinyMovieModel()
+  params: SearchMoviesParams
+
+  async search (params: SearchMoviesParams): Promise<TinyMovieModel[]> {
+    this.params = params
+    return await Promise.resolve(this.movies)
+  }
+}
 
 type SutTypes = {
   sut: RenderResult
   validationSpy: ValidationSpy
+  searchMoviesSpy: SearchMoviesSpy
 }
 
 const makeSut = (): SutTypes => {
   const validationSpy = new ValidationSpy()
-  const sut = render(<Search validation={validationSpy} />)
+  const searchMoviesSpy = new SearchMoviesSpy()
+  const sut = render(<Search validation={validationSpy} searchMovies={searchMoviesSpy} />)
   return {
     sut,
-    validationSpy
+    validationSpy,
+    searchMoviesSpy
   }
 }
 
@@ -56,5 +72,18 @@ describe('Search Component', () => {
     fireEvent.click(submitButton)
     const loading = sut.getByTestId('loading')
     expect(loading).toBeTruthy()
+  })
+
+  test('Should call SearchMovies with correct values', () => {
+    const { sut, validationSpy, searchMoviesSpy } = makeSut()
+    validationSpy.isCorrect = true
+    const searchInput = sut.getByTestId('search')
+    const search = faker.random.word()
+    fireEvent.input(searchInput, { target: { value: search } })
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    fireEvent.click(submitButton)
+    expect(searchMoviesSpy.params).toEqual({
+      s: search
+    })
   })
 })
