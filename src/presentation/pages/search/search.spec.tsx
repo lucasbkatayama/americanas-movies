684a64b4dcf993d0a24b5acce60ce5ba13cff7c9
Lucas Katayama <lucas.katayama@allin.com.br>
2022-10-24 17:44:55 -0300

    test: ensure Search starts with submit button disabled

--- src/presentation/pages/search/search.spec.tsx
+++ src/presentation/pages/search/search.spec.tsx
@@ -3,9 +3,11 @@ import { render } from '@testing-library/react'
 import Search from './search'
 
 describe('Search Component', () => {
-  test('Should not render loading and error on start', () => {
+  test('Should start with initial state', () => {
     const { getByTestId } = render(<Search />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(true)
   })
 })
