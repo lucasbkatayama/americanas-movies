460208bfd9209259b594eabd0d4623c68ad6df65
Lucas Katayama <lucas.katayama@allin.com.br>
2022-10-24 18:04:05 -0300

    refactor: move sut creation to a factory method

--- src/presentation/pages/search/search.spec.tsx
+++ src/presentation/pages/search/search.spec.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Search from './search'
+
+type SutTypes = {
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Search />)
+  return {
+    sut
+  }
+}
 
 describe('Search Component', () => {
   test('Should start with initial state', () => {
-    const { getByTestId } = render(<Search />)
-    const errorWrap = getByTestId('error-wrap')
+    const { sut } = makeSut()
+    const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
-    const submitButton = getByTestId('submit') as HTMLButtonElement
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
   })
 })
