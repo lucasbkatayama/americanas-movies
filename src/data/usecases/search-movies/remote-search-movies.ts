009bb85628ece2b54b367b79bb8194f899cdd16c
Lucas Katayama <lucas.katayama@allin.com.br>
2022-10-23 22:47:20 -0300

    feat: ensure RemoteSearchMovies throws UnexpectedError on default cases

--- src/data/usecases/search-movies/remote-search-movies.ts
+++ src/data/usecases/search-movies/remote-search-movies.ts
@@ -1,6 +1,7 @@
 import { HttpGetClient } from '@/data/protocols/http/http-get-client'
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/errors/invalid-credentials-error'
+import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { SearchMoviesParams } from '@/domain/usecases/search-movies'
 
 export class RemoteSearchMovies {
@@ -16,8 +17,9 @@ export class RemoteSearchMovies {
     })
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok: break
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-      default: return await Promise.resolve()
+      default: throw new UnexpectedError()
     }
   }
 }
