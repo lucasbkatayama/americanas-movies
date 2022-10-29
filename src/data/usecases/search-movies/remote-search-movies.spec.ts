73954142d19ac4769218a2157137615a30066665
Lucas Barbosa Katayama <lucaskatayama12@gmail.com>
2022-10-29 18:37:16 -0300

    test: ensure RemoteSearchMovies throws if response Response is False

--- src/data/usecases/search-movies/remote-search-movies.spec.ts
+++ src/data/usecases/search-movies/remote-search-movies.spec.ts
@@ -1,19 +1,18 @@
-import { RemoteSearchMovies } from './remote-search-movies'
+import { RemoteSearchMovies, RemoteSearchMoviesParams } from './remote-search-movies'
 import { HttpGetClientSpy } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
-import { InvalidCredentialsError, UnexpectedError } from '@/domain/errors'
-import { SearchMoviesParams } from '@/domain/usecases'
-import { TinyMovieModel } from '@/domain/models'
-import { mockSearchMovies, mockTinyMovieModel } from '@/domain/test'
+import { InvalidCredentialsError, NotFoundError, UnexpectedError } from '@/domain/errors'
+import { mockTinyMovieModel } from '@/domain/test'
 import { faker } from '@faker-js/faker'
+import { SearchMoviesResponse } from '@/domain/usecases'
 
 type SutTypes = {
   sut: RemoteSearchMovies
-  httpGetClientSpy: HttpGetClientSpy<SearchMoviesParams, TinyMovieModel[]>
+  httpGetClientSpy: HttpGetClientSpy<RemoteSearchMoviesParams, any>
 }
 
 const makeSut = (url: string = faker.internet.url()): SutTypes => {
-  const httpGetClientSpy = new HttpGetClientSpy<SearchMoviesParams, TinyMovieModel[]>()
+  const httpGetClientSpy = new HttpGetClientSpy<RemoteSearchMoviesParams, SearchMoviesResponse>()
   const sut = new RemoteSearchMovies(url, httpGetClientSpy)
   return {
     sut,
@@ -25,15 +24,34 @@ describe('RemoteSearchMovies', () => {
   test('Should call HttpGetClient with correct URL', async () => {
     const url = 'other_url'
     const { sut, httpGetClientSpy } = makeSut(url)
-    await sut.search(mockSearchMovies())
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: { Response: 'True' }
+    }
+    await sut.search(faker.random.word())
     expect(httpGetClientSpy.url).toBe(url)
   })
 
   test('Should call HttpGetClient with correct params', async () => {
     const { sut, httpGetClientSpy } = makeSut()
-    const searchMoviesParams = mockSearchMovies()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: { Response: 'True' }
+    }
+    const searchMoviesParams = faker.random.word()
     await sut.search(searchMoviesParams)
-    expect(httpGetClientSpy.params).toEqual(searchMoviesParams)
+    expect(httpGetClientSpy.params).toEqual({ s: searchMoviesParams })
+  })
+
+  test('Should throw NotFoundError if response Response returns False', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.ok,
+      body: { Response: 'False' }
+    }
+    const searchMoviesParams = faker.random.word()
+    const promise = sut.search(searchMoviesParams)
+    await expect(promise).rejects.toThrow(new NotFoundError())
   })
 
   test('Should throw InvalidCredentialsError if HttpGetClient returns 401', async () => {
@@ -41,7 +59,7 @@ describe('RemoteSearchMovies', () => {
     httpGetClientSpy.response = {
       statusCode: HttpStatusCode.unauthorized
     }
-    const promise = sut.search(mockSearchMovies())
+    const promise = sut.search(faker.random.word())
     await expect(promise).rejects.toThrow(new InvalidCredentialsError())
   })
 
@@ -50,7 +68,7 @@ describe('RemoteSearchMovies', () => {
     httpGetClientSpy.response = {
       statusCode: HttpStatusCode.badRequest
     }
-    const promise = sut.search(mockSearchMovies())
+    const promise = sut.search(faker.random.word())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
@@ -59,7 +77,7 @@ describe('RemoteSearchMovies', () => {
     httpGetClientSpy.response = {
       statusCode: HttpStatusCode.notFound
     }
-    const promise = sut.search(mockSearchMovies())
+    const promise = sut.search(faker.random.word())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
@@ -68,7 +86,7 @@ describe('RemoteSearchMovies', () => {
     httpGetClientSpy.response = {
       statusCode: HttpStatusCode.serverError
     }
-    const promise = sut.search(mockSearchMovies())
+    const promise = sut.search(faker.random.word())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
@@ -79,7 +97,7 @@ describe('RemoteSearchMovies', () => {
       statusCode: HttpStatusCode.ok,
       body: httpResult
     }
-    const movies = await sut.search(mockSearchMovies())
+    const movies = await sut.search(faker.random.word())
     expect(movies).toEqual(httpResult)
   })
 })
